7d1ef5395955f6c6f4c7e54edb7710a41e6308ce
Squanch <a246004219@gmail.com>
2024-06-08 11:47:18 +0800

    compile ok

---
+++ cmake-build-debug/CMakeFiles/JVMC.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/squanch/CLionProjects/JVMC/ch05/classfile/attr_info.c" "CMakeFiles/JVMC.dir/ch05/classfile/attr_info.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classfile/attr_info.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/classfile/class_file.c" "CMakeFiles/JVMC.dir/ch05/classfile/class_file.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classfile/class_file.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/classfile/class_reader.c" "CMakeFiles/JVMC.dir/ch05/classfile/class_reader.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classfile/class_reader.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/classfile/constant_pool.c" "CMakeFiles/JVMC.dir/ch05/classfile/constant_pool.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classfile/constant_pool.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/classfile/member_info.c" "CMakeFiles/JVMC.dir/ch05/classfile/member_info.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classfile/member_info.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/classpath/entry.c" "CMakeFiles/JVMC.dir/ch05/classpath/entry.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/classpath/entry.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/cmd.c" "CMakeFiles/JVMC.dir/ch05/cmd.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/cmd.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/base/bytecode_reader.c" "CMakeFiles/JVMC.dir/ch05/instructions/base/bytecode_reader.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/base/bytecode_reader.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/base/instruction.c" "CMakeFiles/JVMC.dir/ch05/instructions/base/instruction.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/base/instruction.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/comparisons/cmp.c" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/cmp.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/cmp.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/comparisons/if_acmp.c" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/if_acmp.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/if_acmp.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/comparisons/if_icmp.c" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/if_icmp.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/if_icmp.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/comparisons/ifcond.c" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/ifcond.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/comparisons/ifcond.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/constants/const.c" "CMakeFiles/JVMC.dir/ch05/instructions/constants/const.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/constants/const.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/constants/ipush.c" "CMakeFiles/JVMC.dir/ch05/instructions/constants/ipush.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/constants/ipush.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/constants/ldc.c" "CMakeFiles/JVMC.dir/ch05/instructions/constants/ldc.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/constants/ldc.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/control/goto.c" "CMakeFiles/JVMC.dir/ch05/instructions/control/goto.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/control/goto.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/control/lookupswitch.c" "CMakeFiles/JVMC.dir/ch05/instructions/control/lookupswitch.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/control/lookupswitch.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/control/tableswitch.c" "CMakeFiles/JVMC.dir/ch05/instructions/control/tableswitch.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/control/tableswitch.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/conversions/d2x.c" "CMakeFiles/JVMC.dir/ch05/instructions/conversions/d2x.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/conversions/d2x.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/conversions/i2x.c" "CMakeFiles/JVMC.dir/ch05/instructions/conversions/i2x.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/conversions/i2x.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/extended/ifnull.c" "CMakeFiles/JVMC.dir/ch05/instructions/extended/ifnull.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/extended/ifnull.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/extended/wide.c" "CMakeFiles/JVMC.dir/ch05/instructions/extended/wide.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/extended/wide.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/factory.c" "CMakeFiles/JVMC.dir/ch05/instructions/factory.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/factory.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/loads/iload.c" "CMakeFiles/JVMC.dir/ch05/instructions/loads/iload.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/loads/iload.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/math/boolean.c" "CMakeFiles/JVMC.dir/ch05/instructions/math/boolean.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/math/boolean.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/math/iinc.c" "CMakeFiles/JVMC.dir/ch05/instructions/math/iinc.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/math/iinc.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/math/rem.c" "CMakeFiles/JVMC.dir/ch05/instructions/math/rem.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/math/rem.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/math/sh.c" "CMakeFiles/JVMC.dir/ch05/instructions/math/sh.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/math/sh.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/reference/new.c" "CMakeFiles/JVMC.dir/ch05/instructions/reference/new.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/reference/new.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/stack/dup.c" "CMakeFiles/JVMC.dir/ch05/instructions/stack/dup.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/stack/dup.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/stack/pop.c" "CMakeFiles/JVMC.dir/ch05/instructions/stack/pop.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/stack/pop.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/stack/swap.c" "CMakeFiles/JVMC.dir/ch05/instructions/stack/swap.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/stack/swap.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/instructions/stores/lstore.c" "CMakeFiles/JVMC.dir/ch05/instructions/stores/lstore.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/instructions/stores/lstore.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/interpret.c" "CMakeFiles/JVMC.dir/ch05/interpret.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/interpret.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/rtda/heap/class.c" "CMakeFiles/JVMC.dir/ch05/rtda/heap/class.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/rtda/heap/class.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/rtda/heap/object.c" "CMakeFiles/JVMC.dir/ch05/rtda/heap/object.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/rtda/heap/object.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/ch05/rtda/thread.c" "CMakeFiles/JVMC.dir/ch05/rtda/thread.c.o" "gcc" "CMakeFiles/JVMC.dir/ch05/rtda/thread.c.o.d"
+  "/Users/squanch/CLionProjects/JVMC/main.c" "CMakeFiles/JVMC.dir/main.c.o" "gcc" "CMakeFiles/JVMC.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
